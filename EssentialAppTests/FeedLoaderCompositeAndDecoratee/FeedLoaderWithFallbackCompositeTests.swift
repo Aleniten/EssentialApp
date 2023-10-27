306ea58c03850d8d65e6dbb7872d51779d711870
Aleniten <jaherrero85@gmail.com>
2023-10-27 10:00:51 -0300

    Extract system under test (SUT) into a factory method

--- EssentialAppTests/FeedLoaderCompositeAndDecoratee/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialAppTests/FeedLoaderCompositeAndDecoratee/FeedLoaderWithFallbackCompositeTests.swift
@@ -34,7 +34,7 @@ final class FeedLoaderWithFallbackCompositeTests: XCTestCase, FeedLoaderTestCase
     
         // MARK: Helper
     
-        private func makeSUT(primaryResult: FeedLoader.Result, fallbackResult: FeedLoader.Result,file: StaticString = #filePath, line: UInt = #line) -> FeedLoader {
+        private func makeSUT(primaryResult: FeedLoader.Result, fallbackResult: FeedLoader.Result, file: StaticString = #filePath, line: UInt = #line) -> FeedLoader {
             let primaryLoader = FeedLoaderStub(result: primaryResult)
             let fallbackLoader = FeedLoaderStub(result: fallbackResult)
             let sut = FeedLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)

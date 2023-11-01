45af9deff7fcaf63eb14dc8e4b946d20aa5bcb77
Aleniten <jaherrero85@gmail.com>
2023-11-01 14:12:05 -0300

    Remove unnecesary main storyboard the initial setup happens in code

--- EssentialAppTests/SceneDelegateTests.swift
+++ EssentialAppTests/SceneDelegateTests.swift
@@ -11,6 +11,15 @@ import EssentialFeediOS
 
 final class SceneDelegateTests: XCTestCase {
 
+    func test_configureWindow_setsWindowAsKeyAndVisible() {
+        let window = UIWindowSpy()
+        let sut = SceneDelegate()
+        sut.window = window
+        
+        sut.configureWindow()
+        XCTAssertEqual(window.makeKeyAndVisibleCallCount, 1, "Expected to make window key and visible")
+    }
+    
     func test_sceneWillConnectToSession_configureRootViewController() {
         let sut = SceneDelegate()
         sut.window = UIWindow()
@@ -23,4 +32,11 @@ final class SceneDelegateTests: XCTestCase {
         XCTAssertNotNil(rootNavigation, "Expected  navigation controller as root, got instead \(String(describing: root))")
         XCTAssertTrue(topController is FeedViewController, "Expected a FeedViewController as a topController, got instead \(String(describing: topController))")
     }
+}
+
+private class UIWindowSpy: UIWindow {
+  var makeKeyAndVisibleCallCount = 0
+  override func makeKeyAndVisible() {
+    makeKeyAndVisibleCallCount = 1
+  }
 }
